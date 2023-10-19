8e1525038adb783fe94fbe4988207ae5f4d5dc48
Partogi Simatupang <untukgamep1@gmail.com>
2023-10-19 16:25:44 +0700

    Flutter Apps for PSA

---
+++ trafindo/android/app/src/main/kotlin/com/trafoindopsa/trafindo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.trafoindopsa.trafindo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ trafindo/lib/walkthrough/walkthrough_image.dart
@@ -0,0 +1,5 @@
+const walkThroughImage1 = 'assets/images/walkthrough-2.png';
+const walkThroughImage2 = 'assets/images/walkthrough-5.png';
+const walkThroughImage3 = 'assets/images/walkthrough-4.png';
+const walkThroughImage4 = 'assets/images/walkthrough-1.png';
+const walkThroughImage5 = 'assets/images/walkthrough-3.png';
